4f78ee286d909611093ce4c5a5be78578ffa2e88
Nickname8012 <shivsao1111@gmail.com>
2025-04-27 02:06:04 +0530

    Updated for deployment settings

--- app.py
+++ app.py
@@ -68,4 +68,5 @@ def chat():
         })
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    port = int(os.environ.get('PORT', 5000))  # <- dynamic port for deployment
+    app.run(host='0.0.0.0', port=port, debug=True)  # <- 0.0.0.0 binding
